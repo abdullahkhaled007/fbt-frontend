7fa437e513fc30287d1f4cc3d2027e395b6cc9ee
abdullahkhaled007 <abdullah_khaled@hotmail.co.uk>
2022-03-17 19:43:31 +0000

    mockup using react

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/pizza.jpeg";
+import "../styles/Home.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
